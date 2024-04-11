82705a71fcd6df1c9339c2d5d5fd19c0702c35ea
German Salinas <germansalinas.fce@gmail.com>
2024-04-11 17:07:27 -0300

    Parte 2.6 Creacion de superusuario

--- Tp0/polls/admin.py
+++ Tp0/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# importo el modelo Question para poder registrarlo en el administrador
+from .models import Question
+
+# registro de la aplicación Question en el administrador esto permite 
+# que se pueda administrar desde el panel de administración de Django
+admin.site.register(Question)

531865fd7255d199604b6ae4d1d50bd78481f129
German Salinas <germansalinas.fce@gmail.com>
2024-04-13 00:11:27 -0300

    Parte 3.1 Agrego vista detalle, resultado y voto con sus urls

--- Tp0/polls/urls.py
+++ Tp0/polls/urls.py
@@ -6,4 +6,11 @@ from . import views
 # defino la ruta de mi aplicación, en este caso la raíz, y le asigno la vista index, con nombre index
 urlpatterns = [
     path('', views.index, name='index'),
+    # defino la ruta detail con el parametro question_id y le asigno la vista detail, con nombre detail
+    path('<int:question_id>/', views.detail, name='detail'),
+    # defino la ruta results con el parametro question_id y le asigno la vista results, con nombre results
+    path('<int:question_id>/results/', views.results, name='results'),
+    # defino la ruta vote con el parametro question_id y le asigno la vista vote, con nombre vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    
 ]
--- Tp0/polls/views.py
+++ Tp0/polls/views.py
@@ -4,3 +4,19 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hola mundo, esta es mi primera vista en Django!")
+
+# esta funcion recibe como parametro el question_id que se pasa por la url
+# y devuelve un mensaje con el id de la pregunta
+def detail(request, question_id):
+    return HttpResponse("Estas mirando la pregunta %s." % question_id)
+
+# esta funcion recibe como parametro el question_id que se pasa por la url
+# y devuelve un mensaje con el id de la pregunta para indicar que se estan viendo los resultados
+def results(request, question_id):
+    response = "Estas viendo los resultados de la pregunta %s."
+    return HttpResponse(response % question_id)
+
+# esta funcion recibe como parametro el question_id que se pasa por la url
+# y devuelve un mensaje con el id de la pregunta para indicar que se esta votando
+def vote(request, question_id):
+    return HttpResponse("Estas votando la pregunta %s." % question_id)

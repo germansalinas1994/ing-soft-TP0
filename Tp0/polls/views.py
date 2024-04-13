71557b8bc1c7120f438fb4e531caad2e8bc5164a
German Salinas <germansalinas.fce@gmail.com>
2024-04-13 10:25:30 -0300

    Parte 4.2 Creacion de la view de resultados

--- Tp0/polls/views.py
+++ Tp0/polls/views.py
@@ -19,8 +19,9 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {"question": question})
 
 def results(request, question_id):
-    response = "Estas viendo los resultados de la pregunta %s." % question_id
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
